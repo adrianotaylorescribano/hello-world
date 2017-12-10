419735394f94a2ce2393ef73c41df6f21d9198e1
adrianotaylorescribano <34423562+adrianotaylorescribano@users.noreply.github.com>
2017-12-10 15:07:07 -0500

    x2 hello world
    adding second method hello world

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,14 @@
 def do_this_method():
   hi = "hello world"
   print(hi)
+  
+def do_this_method_too():
+  hi2 = "hello world part 2"
+  print(hi2)
 
 
 if __name__ == "__main__":
   
   do_this_method()
+  
+  do_this_method_too()
